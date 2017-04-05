e05f7e346ae6ef3e63cfba375be43ddbfcb77ac2
Jack Cherng <jfcherng@gmail.com>
2017-04-05 18:44:34 +0800

    nits

--- do_lookup.bat
+++ do_lookup.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
 REM The department ID of NTHU EE "Jia" Group
-SET department_A=011322
+SET department_NthuEe_Jia=011322
 REM The department ID of NTHU EE "Yi" Group
-SET department_B=011332
+SET department_NthuEe_Yi=011332
 
-python lookup.py --outputFormat="NthuEe" --output="NTHU-EE-A.csv" --departmentIds=%department_A%
-python lookup.py --outputFormat="NthuEe" --output="NTHU-EE-B.csv" --departmentIds=%department_B%
-python lookup.py --outputFormat="NthuEe" --output="NTHU-EE-AB.csv" --departmentIds=%department_A%,%department_B%
+python lookup.py --outputFormat="NthuEe" --output="NTHU-EE-A.csv" --departmentIds=%department_NthuEe_Jia%
+python lookup.py --outputFormat="NthuEe" --output="NTHU-EE-B.csv" --departmentIds=%department_NthuEe_Yi%
+python lookup.py --outputFormat="NthuEe" --output="NTHU-EE-AB.csv" --departmentIds=%department_NthuEe_Jia%,%department_NthuEe_Yi%
 
 PAUSE
