44afdf288c12032cb4523c1e8a29a4684fff4ac1
Jack Cherng <jfcherng@gmail.com>
2020-04-05 03:26:48 +0800

    fix: update for 109
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- 第一階段-篩選結果（甄選委員會）/do_crawl.bat
+++ 第一階段-篩選結果（甄選委員會）/do_crawl.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 REM The (base) URL of the CAAC HTML page
-SET projectBaseUrl="https://www.cac.edu.tw/CacLink/apply108/108apply_SieveR_erg95fs/html_sieve_coco108/ColPost/collegeList.htm"
+SET projectBaseUrl="https://www.cac.edu.tw/CacLink/apply109/109apply_6pSieveXw_ju9sd98/html_sieve_109pgx3/ColPost/collegeList.htm"
 
 python crawler.py --projectBaseUrl="%projectBaseUrl%"
 
