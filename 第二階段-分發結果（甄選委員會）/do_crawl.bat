96e94f6356135491d8dc4fce0bd85442b9a40cca
Jack Cherng <jfcherng@gmail.com>
2020-05-21 12:46:29 +0800

    chore: update entrance link for 2019
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- 第二階段-分發結果（甄選委員會）/do_crawl.bat
+++ 第二階段-分發結果（甄選委員會）/do_crawl.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 REM The (base) URL of the CAAC HTML page
-SET projectBaseUrl="https://www.cac.edu.tw/CacLink/apply108/108apply_kg_Entrance_fq6ryh7/html_entrance_kk108nonp/result_html/result_apply/collegeList.htm"
+SET projectBaseUrl="https://www.cac.edu.tw/CacLink/apply109/109apply_cP5_Entrance_r6f57a5Fm/html_entrance_5R109hlw/result_html/result_apply/collegeList.htm"
 
 python crawler.py --projectBaseUrl="%projectBaseUrl%"
 
