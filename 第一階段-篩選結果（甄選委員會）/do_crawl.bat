3e88b19726a9f30d28f7cf6eb1673094ed509b62
Jack Cherng <jfcherng@gmail.com>
2022-04-01 02:04:48 +0800

    chore: update info for year 111
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- 第一階段-篩選結果（甄選委員會）/do_crawl.bat
+++ 第一階段-篩選結果（甄選委員會）/do_crawl.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 REM The (base) URL of the CAAC HTML page
-SET projectBaseUrl="https://www.cac.edu.tw/CacLink/apply110/110apply_pgSieve_22sd8rga/html_sieve_110swk5m/ColPost/collegeList.htm"
+SET projectBaseUrl="https://www.cac.edu.tw/CacLink/apply111/111apply_kSieveOD_8w4Qas6g/html_sieve_111psHkq5/ColPost/collegeList.htm"
 
 python crawler.py --projectBaseUrl="%projectBaseUrl%"
 
